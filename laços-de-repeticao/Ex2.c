1635c3806bb78e8f3a1b85b0d638486bec6d8706
Julia Ayomide <145088749+juliaayomide@users.noreply.github.com>
2026-09-16 21:33:05 -0300

    Create Ex2.c
    Programa que com base no número inserido correspondente a uma fruta retorna o valor do quilo (usando switch case).

---
+++ laços-de-repeticao/Ex2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+	int opcao;
+	
+	printf("Informe a opcao que deseja para obter o valor do quilo de cada fruta \n");
+	printf("Opcao 1: Banana | Opcao 2: Maca | Opcao 3: Laranja | Opcao 4: Mamao \n");
+	scanf("%d",&opcao);
+	
+	switch (opcao) {
+		case 1:
+		printf("O quilo da Banana esta RS5.00 (5 reais)");
+		break;
+		case 2:
+			printf("O quilo da Maca esta RS6.00 (6 reais)");
+		break;
+		case 3:
+		printf("O quilo da Laranja esta RS4.00 (4 reais)");
+		break;
+		case 4: 
+		printf("O quilo do Mamao esta RS5.50 (5 reais e cinquenta centavos)");
+		break;
+		default:
+		printf("Opcao invalida, insira outra!");
+		
+	}
+	
+	return 0;
+}
